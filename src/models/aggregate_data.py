8ba762cc14055f0b14dfd7ff56fd204769b9fd8c
rosieyzh <rosieyzh@gmail.com>
2019-08-30 13:14:19 -0400

    Aggregated all left and right articles in two csvs

--- src/models/aggregate_data.py
+++ src/models/aggregate_data.py
@@ -1,29 +1,32 @@
 import pandas as pd
 
-def agg_data(files):
+def agg_data(files, saveFile):
 	li=[]
 	for filename in files:
 		df = pd.read_csv(filename, index_col=None, header=0)
 		li.append(df)
 	
 	all_data = pd.concat(li, axis=0, ignore_index=True)
-	all_data.to_csv('../../data/all_data.csv')
+	all_data.to_csv('../../data/{}.csv'.format(saveFile))
 
 if __name__ == '__main__':
-	files = [
+	right = [
 			'../../data/right/data_climate_depot.csv',
 			'../../data/right/data_fox_articles.csv',
 			'../../data/right/data_global_climate_scam.csv',
 			'../../data/right/data_breitbart.csv',
-			'../../data/right/data_wattsup.csv',
+			'../../data/right/data_wattsup.csv'
+			]
+	left = [
 			'../../data/left/data_abc_articles.csv',
 			'../../data/left/data_alternet_articles.csv',
 			'../../data/left/data_cbs_articles.csv',
 			'../../data/left/data_democracynow_articles.csv',
-			'../../data/left/mother_jones_articles.csv',
-			'../../data/left/data_inside_climate_news.csv'
-			]
+			'../../data/left/data_mj_articles.csv',
+			'../../data/left/data_inside_climate_news_articles.csv'
+		   ]
 
-	agg_data(files)
+	agg_data(right, 'all_right')
+	agg_data(left, 'all_left')
 
 
